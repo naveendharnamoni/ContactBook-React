8c5b7d86df37623d261c74254e807b8436e162e6
Naveen Dharnamoni <dharnamoninaveen@gmail.com>
2019-11-11 11:59:26 +0530

    refactored and removed unnecessary methods

--- src/containers/MainContent/MainContent.js
+++ src/containers/MainContent/MainContent.js
@@ -5,114 +5,36 @@ import ContactInfo from "../../components/Contacts/ContactInfo/ContactInfo";
 import ContactModel from "../../Models/ContactModel";
 import Navbar from "../../components/Navbar/Navbar";
 import {connect} from 'react-redux';
+import * as actionTypes from '../../store/actions';
 
 class MainContent extends Component {
-  state = {
-    contacts: [
-      new ContactModel({
-        id: 1,
-        name: "naveen",
-        email: "naveen@gmail.com",
-        mobile: 9533466343,
-        landline: "040 ",
-        website: "nav.com",
-        address: "shantinagar"
-      })
-    ],
-    isContactInfo: false,
-    isFormDisplayed: false,
-    contactSelected: null
-  };
-
-  toggleContactForm = () => {
-    this.setState({
-      isFormDisplayed: !this.state.isFormDisplayed
-    });
-  };
-
-  toggleContactInfo = id => {
-    let shouldDisplayCntctInfo = true;
-    if (id === this.state.contactSelected && this.state.isContactInfo) {
-      shouldDisplayCntctInfo = false;
-    }
-    this.setState({
-      isContactInfo: shouldDisplayCntctInfo,
-      contactSelected: id
-    });
-  };
 
   selectedContact() {
-    return this.state.contacts.find(x => x.id === this.state.contactSelected);
+    return this.props.contacts.find(x => x.id === this.props.contactSelected);
   }
-
-  addOrEditContact = contact => {
-    let contacts = this.state.contacts;
-    let existingContact = contacts.find(
-      x => x.id === this.state.contactSelected
-    );
-    if (existingContact) {
-      existingContact.name = contact.name;
-      existingContact.email = contact.email;
-      existingContact.mobile = contact.mobile;
-      existingContact.landline = contact.landline;
-      existingContact.address = contact.address;
-    } else {
-      contact.id = contacts.length + 1;
-      contacts.push(contact);
-    }
-    this.setState({
-      contacts: contacts
-    });
-    this.toggleContactForm();
-  };
-
-  deleteContact = id => {
-    let contacts = [...this.state.contacts];
-    const contact = contacts.find(x => x.id === id);
-    contacts.pop(contact);
-    this.setState({
-      contacts: contacts
-    });
-    this.toggleContactInfo(null);
-    alert("contact has been deleted");
-
-    // if (confirm("Do you want to delete")) {
-
-    // }
-  };
-
-  editContact = () => {
-    this.toggleContactForm();
-  };
-  addContact = () => {
-    this.setState({
-      contactSelected: null
-    });
-    this.props.toggleContactForm();
-  };
 
   render() {
     return (
       <Fragment>
-        <Navbar clicked={this.addContact}></Navbar>
+        <Navbar clicked={this.props.addContact}></Navbar>
         <h2 className="cntct-heading">Contacts</h2>
         <div className="container">
           <Contacts
             contacts={this.props.contacts}
-            displayContactInfo={this.toggleContactInfo}
+            displayContactInfo={this.props.toggleContactInfo}
           ></Contacts>
           {this.props.isFormDisplayed ? (
             <ContactForm
               toggleForm={this.props.toggleContactForm}
-              addContact={this.addOrEditContact}
+              addContact={this.props.addOrEditContact}
               {...this.selectedContact()}
             ></ContactForm>
           ) : null}
-          {this.state.isContactInfo ? (
+          {this.props.isContactInfo ? (
             <ContactInfo
               {...this.selectedContact()}
-              delete={this.deleteContact}
-              edit={this.editContact}
+              delete={this.props.deleteContact}
+              edit={this.props.editContact}
             ></ContactInfo>
           ) : null}
         </div>
@@ -132,7 +54,12 @@ const mapStateToProps = state => {
 
 const mapDispatchToProps = dispatch =>{
   return{
-    toggleContactForm : () => dispatch({type: "TOGGLE_FORM"})
+    toggleContactForm : () => dispatch({type: actionTypes.TOGGLE_FORM}),
+    addContact : () => dispatch({type: actionTypes.ADD_CONTACT}),
+    editContact : () => dispatch({type: actionTypes.EDIT_CONTACT}),
+    addOrEditContact : (contact) => dispatch({type: actionTypes.ADD_EDIT_CONTACT,payload:contact}),
+    deleteContact : (id) => dispatch({type: actionTypes.DELETE_CONTACT,payload:id}),
+    toggleContactInfo : (id) => dispatch({type: actionTypes.TOGGLE_CONTACT_INFO,id:id}),
   }
 }
 
