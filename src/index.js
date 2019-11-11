36c5a4fc8a33f0ac18b953ae52f30fdc9780bc18
Naveen Dharnamoni <dharnamoninaveen@gmail.com>
2019-11-11 10:46:00 +0530

    added enhancer for debugging

--- src/index.js
+++ src/index.js
@@ -3,12 +3,14 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import {createStore } from 'redux'
+import {createStore,compose } from 'redux'
 import reducer from './store/reducer';
 import {Provider} from 'react-redux';
 
 
-const store = createStore(reducer);
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const store = createStore(reducer, composeEnhancers());
 
 ReactDOM.render(<Provider store = {store}><App /></Provider>, document.getElementById('root'));
 
